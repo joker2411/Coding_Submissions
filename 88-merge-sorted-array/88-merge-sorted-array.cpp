c2ff03b6e28480573dce8e4e0d227aaa22ea0429
AMBUJ MISHRA <ambuj.mishra.2k12@gmail.com>
2022-06-27 18:29:27 +0530

    Time: 4 ms (52.17%), Space: 8.9 MB (99.17%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> f;
-        int i=0, j=0;
-        while(i<m && j<n){
-            if(nums1[i] <= nums2[j]){
-                f.push_back(nums1[i]);
-                i++;
+        int i=m-1;
+        int j=n-1;
+        int k=m+n-1;
+        
+        while(i >= 0 && j >= 0){
+            if(nums1[i] >= nums2[j]){
+                nums1[k] = nums1[i];
+                k--;
+                i--;
             }
             else{
-                f.push_back(nums2[j]);
-                j++;
+                nums1[k] = nums2[j];
+                k--;
+                j--;
             }
         }
-        if(i == m){
-            while(j<n){
-                f.push_back(nums2[j]);
-                j++;
-            }
+        while(j >= 0){
+            nums1[k] = nums2[j];
+            k--;
+            j--;
         }
-        else{
-            while(i<m){
-                f.push_back(nums1[i]);
-                i++;
-            }
-        }
-        
-        for(i=0; i<m+n; i++){
-            nums1[i] = f[i];
-        }
+        return;
     }
 };
