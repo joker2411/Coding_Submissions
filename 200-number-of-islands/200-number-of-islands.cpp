f89701e3eb58061b20e43a716b8bf03a0a72e53f
AMBUJ MISHRA <ambuj.mishra.2k12@gmail.com>
2022-08-29 13:53:09 +0530

    Time: 57 ms (49.65%), Space: 12.5 MB (44.10%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,47 +1,36 @@
 class Solution {
 public:
-    bool checkCordinates(int x, int y, int N, int M){
-        if((x>=0 && x<N) && (y>=0 && y<M))
-            return true;
-        return false;
-    }
-    
-    void visitIslandwithDFS(vector<vector<char>>& grid, vector<vector<bool>>& visited, int N, int M, int i, int j){
+    void DFS(vector<vector<char>>& grid, vector<vector<bool>>& visited, int i, int j, int M, int N){
+        if(i<0 || i>=M || j<0 || j>=N || grid[i][j] == '0' || visited[i][j] == true)
+            return;
+        
         visited[i][j] = true;
         
-        if(checkCordinates(i-1,j, N, M) && visited[i-1][j] == false && grid[i-1][j] == '1')
-            visitIslandwithDFS(grid, visited, N, M, i-1, j);
-        if(checkCordinates(i+1,j, N, M) && visited[i+1][j] == false && grid[i+1][j] == '1')
-            visitIslandwithDFS(grid, visited, N, M, i+1, j);
-        if(checkCordinates(i,j-1, N, M) && visited[i][j-1] == false && grid[i][j-1] == '1')
-            visitIslandwithDFS(grid, visited, N, M, i, j-1);
-        if(checkCordinates(i,j+1, N, M) && grid[i][j+1] == '1')
-            visitIslandwithDFS(grid, visited, N, M, i, j+1);
-    }
-    
-    int checkIslands(vector<vector<char>>& grid){
-        int N = grid.size();
-        int M = grid[0].size();
-        
-        vector<vector<bool>> visited(N, vector<bool>(M, false));
-        
-        int ans = 0;
-        for(int i=0; i<N; i++){
-            for(int j=0; j<M; j++){
-                if(visited[i][j] == true)
-                    continue;
-                else if(grid[i][j] == '1'){
-                    visitIslandwithDFS(grid, visited, N, M, i, j);
-                    ans++;
-                }
-                else
-                    visited[i][j] = true;
-            }
-        }
-        return ans;
+        DFS(grid, visited, i+1, j, M, N);
+        DFS(grid, visited, i-1, j, M, N);
+        DFS(grid, visited, i, j+1, M, N);
+        DFS(grid, visited, i, j-1, M, N);
+        return;
     }
     
     int numIslands(vector<vector<char>>& grid) {
-        return checkIslands(grid);
+        int M = grid.size();
+        int N = grid[0].size();
+        
+        vector<vector<bool>> visited(M, vector<bool>(N, false));
+        
+        int ans = 0;
+        for(int i=0; i<M; i++){
+            for(int j=0; j<N; j++){
+                if(visited[i][j] == true)
+                    continue;
+                
+                if(grid[i][j] == '1'){
+                    DFS(grid, visited, i, j, M, N);
+                    ans++;
+                }
+            }
+        }
+        return ans;
     }
 };
